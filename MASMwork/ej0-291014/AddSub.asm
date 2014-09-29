e615e926d86bb0aa64a87b4d4b425b83779d71c4
unknown <hca@CC102-19.itam.mx>
2014-09-29 08:09:00 -0500

    Compiled first file

---
+++ MASMwork/ej0-291014/AddSub.asm
@@ -0,0 +1,18 @@
+TITLE Add and Subtract              (AddSub.asm)
+
+; This program adds and subtracts 32-bit integers.
+
+
+INCLUDE Irvine32.inc
+
+.CODE
+main PROC
+
+	MOV EAX,10000h		; EAX = 10000h
+	ADD EAX,40000h		; EAX = 50000h
+	SUB EAX,20000h		; EAX = 30000h
+	CALL DumpRegs
+
+	EXIT
+main ENDP
+END main
