7aee6f37e51e554fd82885e45936db9bc774a61b
Luis Mendoza <luis@mendoza.io>
2014-10-01 08:51:24 -0500

    Symbols Example

--- MASMwork/ej04-011014/AddSub2.asm
+++ MASMwork/ej04-011014/AddSub2.asm
@@ -7,22 +7,27 @@ TITLE Add and Subtract, Version 2         (AddSub2.asm)
 INCLUDE Irvine32.inc
 
 .DATA
+
+inicio = $
 val1     DWORD  10000h
+tamano = ($ - inicio)
 val2     DWORD  40000h
 val3     DWORD  20000h
 finalVal DWORD  ?
+cuenta = ($ - inicio) / tamano
+
 
 .CODE
 main PROC
 
-	MOV EAX,val1	; start with 10000h
-	ADD EAX,val2	; add 40000h
-	SUB EAX,val3	; subtract 20000h
-	MOV finalVal,EAX	; store the result (30000h)
-	CALL DumpRegs	; display the registers
+	MOV EAX,val1			; start with 10000h
+	ADD EAX,val2			; add 40000h
+	SUB EAX,val3			; subtract 20000h
+	MOV finalVal,EAX		; store the result (30000h)
+	CALL DumpRegs			; display the registers
 	MOV ESI, OFFSET val1	; move val1 pointer to ES1
-	MOV ECX, 4
-	MOV EBX, 4
+	MOV ECX, cuenta			; 4 simbolos a leer
+	MOV EBX, tamano			; cada uno de longitud 4 bytes
 	CALL DumpMem
 
 	EXIT
